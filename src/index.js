75db2eb682b8f8036b49aadada2288052c149f35
xianshannan <xianshannan@qq.com>
2017-08-16 19:43:08 +0800

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import BrowserProvider from './browser-provider';
+import HashProvider from './hash-provider';
+import MemoryProvider from './memory-provider';
+
+export { BrowserProvider, HashProvider, MemoryProvider };
