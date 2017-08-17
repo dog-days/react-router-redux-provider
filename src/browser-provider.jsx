e766d004005be01f1250283941ec824f9d7a7734
xianshannan <xianshannan@qq.com>
2017-08-17 17:50:44 +0800

    fix history 参数没传进去问题

--- src/browser-provider.jsx
+++ src/browser-provider.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Provider from './provider';
 import createHistory from 'history/createBrowserHistory';
 
 export default class BrowserProvider extends React.Component {
+  static propTypes = {
+    basename: PropTypes.string,
+    forceRefresh: PropTypes.bool,
+    getUserConfirmation: PropTypes.func,
+    keyLength: PropTypes.number,
+  };
   displayName = 'BrowserProvider';
-  history = createHistory();
+  history = createHistory(this.props);
   render() {
     const { children, history = this.history, ...others } = this.props;
     return (
--- src/memory-provider.jsx
+++ src/memory-provider.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Provider from './provider';
 import createHistory from 'history/createMemoryHistory';
 
 export default class MemoryProvider extends React.Component {
+  static propTypes = {
+    basename: PropTypes.string,
+    forceRefresh: PropTypes.bool,
+    getUserConfirmation: PropTypes.func,
+    keyLength: PropTypes.number,
+  };
   displayName = 'MemoryProvider';
-  history = createHistory();
+  history = createHistory(this.props);
   render() {
     const { children, history = this.history, ...others } = this.props;
     return (
